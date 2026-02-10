b6e8b5d580232cf4b8571b89cfca34ae146a24c6
Victor Garcia Mestre <vgmestre@gmail.com>
2026-02-10 20:12:04 +0100

    feat: new post

--- .source/server.ts
+++ .source/server.ts
@@ -1,14 +1,15 @@
 // @ts-nocheck
-import * as __fd_glob_15 from "../src/content/writing/vercel-relative-time-card.mdx?collection=posts"
-import * as __fd_glob_14 from "../src/content/writing/upload-images-with-rails-and-cloudinary.mdx?collection=posts"
-import * as __fd_glob_13 from "../src/content/writing/understanding-react-server-components.mdx?collection=posts"
-import * as __fd_glob_12 from "../src/content/writing/terminal-setup-2025.mdx?collection=posts"
-import * as __fd_glob_11 from "../src/content/writing/spotify-filters.mdx?collection=posts"
-import * as __fd_glob_10 from "../src/content/writing/sparkling-text.mdx?collection=posts"
-import * as __fd_glob_9 from "../src/content/writing/scratch-card.mdx?collection=posts"
-import * as __fd_glob_8 from "../src/content/writing/powershell-custom-prompt-setup.mdx?collection=posts"
-import * as __fd_glob_7 from "../src/content/writing/mise-as-nvm-replacement.mdx?collection=posts"
-import * as __fd_glob_6 from "../src/content/writing/how-shadcn-documentation-is-built.mdx?collection=posts"
+import * as __fd_glob_16 from "../src/content/writing/vercel-relative-time-card.mdx?collection=posts"
+import * as __fd_glob_15 from "../src/content/writing/upload-images-with-rails-and-cloudinary.mdx?collection=posts"
+import * as __fd_glob_14 from "../src/content/writing/understanding-react-server-components.mdx?collection=posts"
+import * as __fd_glob_13 from "../src/content/writing/terminal-setup-2025.mdx?collection=posts"
+import * as __fd_glob_12 from "../src/content/writing/spotify-filters.mdx?collection=posts"
+import * as __fd_glob_11 from "../src/content/writing/sparkling-text.mdx?collection=posts"
+import * as __fd_glob_10 from "../src/content/writing/scratch-card.mdx?collection=posts"
+import * as __fd_glob_9 from "../src/content/writing/powershell-custom-prompt-setup.mdx?collection=posts"
+import * as __fd_glob_8 from "../src/content/writing/mise-as-nvm-replacement.mdx?collection=posts"
+import * as __fd_glob_7 from "../src/content/writing/how-shadcn-documentation-is-built.mdx?collection=posts"
+import * as __fd_glob_6 from "../src/content/writing/going-analog.mdx?collection=posts"
 import * as __fd_glob_5 from "../src/content/writing/flip-dot-matrix.mdx?collection=posts"
 import * as __fd_glob_4 from "../src/content/writing/custom-code-blocks-astro-mdx.mdx?collection=posts"
 import * as __fd_glob_3 from "../src/content/writing/build-your-own-react-router.mdx?collection=posts"
@@ -23,4 +24,4 @@ const create = server<typeof Config, import("fumadocs-mdx/runtime/types").Intern
   }
 }>({"doc":{"passthroughs":["extractedReferences"]}});
 
-export const posts = await create.doc("posts", "src/content/writing", {"adding-likes-to-static-site.mdx": __fd_glob_0, "animated-tabs.mdx": __fd_glob_1, "astro-fonts-api-optimization.mdx": __fd_glob_2, "build-your-own-react-router.mdx": __fd_glob_3, "custom-code-blocks-astro-mdx.mdx": __fd_glob_4, "flip-dot-matrix.mdx": __fd_glob_5, "how-shadcn-documentation-is-built.mdx": __fd_glob_6, "mise-as-nvm-replacement.mdx": __fd_glob_7, "powershell-custom-prompt-setup.mdx": __fd_glob_8, "scratch-card.mdx": __fd_glob_9, "sparkling-text.mdx": __fd_glob_10, "spotify-filters.mdx": __fd_glob_11, "terminal-setup-2025.mdx": __fd_glob_12, "understanding-react-server-components.mdx": __fd_glob_13, "upload-images-with-rails-and-cloudinary.mdx": __fd_glob_14, "vercel-relative-time-card.mdx": __fd_glob_15, });
+export const posts = await create.doc("posts", "src/content/writing", {"adding-likes-to-static-site.mdx": __fd_glob_0, "animated-tabs.mdx": __fd_glob_1, "astro-fonts-api-optimization.mdx": __fd_glob_2, "build-your-own-react-router.mdx": __fd_glob_3, "custom-code-blocks-astro-mdx.mdx": __fd_glob_4, "flip-dot-matrix.mdx": __fd_glob_5, "going-analog.mdx": __fd_glob_6, "how-shadcn-documentation-is-built.mdx": __fd_glob_7, "mise-as-nvm-replacement.mdx": __fd_glob_8, "powershell-custom-prompt-setup.mdx": __fd_glob_9, "scratch-card.mdx": __fd_glob_10, "sparkling-text.mdx": __fd_glob_11, "spotify-filters.mdx": __fd_glob_12, "terminal-setup-2025.mdx": __fd_glob_13, "understanding-react-server-components.mdx": __fd_glob_14, "upload-images-with-rails-and-cloudinary.mdx": __fd_glob_15, "vercel-relative-time-card.mdx": __fd_glob_16, });
